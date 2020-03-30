668e35b3c52f8da74f0e715d8cbcc5b71693889a
Sajeeb <sajeebahamed6@gmail.com>
2020-03-31 04:56:16 +0600

    get

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Is server open or closed??");
+    const students = {
+        name : "Sajeeb",
+        id  : 1,
+        sec : "A",
+        dept : "CSE"
+    }
+    res.send(students);
 })
-
+app.get('/fruits/banna', (req, res) => {
+    res.send({fruit: 'banana', quantity: 100, pricew : 500});
+})
+const users = ['John', 'Cameron', 'Shunny', 'Hui Hui']
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+})
 app.listen(4200, () => console.log('Listening to the port 4200'));
