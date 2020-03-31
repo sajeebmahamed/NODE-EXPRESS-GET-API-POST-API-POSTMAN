4bc16be3dbb0c7dea214f712724b419782593a83
Sajeeb <sajeebahamed6@gmail.com>
2020-04-01 01:42:06 +0600

    external html files added

--- index.js
+++ index.js
@@ -33,3 +33,58 @@ app.post('/addUser', (req, res) => {
     res.send(user);
 });
 app.listen(4200, () => console.log('Listening to the port 4200'));
+
+/* 
+
+====================External Html FIle=============================
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4 id="userName">Show Data</h4>
+    <div>
+        <input type="text" id="uuser">
+        <button id="btnUser">Add User</button>
+    </div>
+    <script>
+        fetch('http://localhost:4200/users/1')
+        .then(res => res.json())
+        .then(data=>{
+            document.getElementById('userName').innerText = data.name;
+        });
+
+
+
+        // post data to server
+
+        const btn = document.getElementById('btnUser');
+        btn.addEventListener('click', () =>{
+            const name = document.getElementById('uuser').value;
+            const user = {name};
+            fetch('http://localhost:4200/addUser', {
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-type" : "application/json; charset=UTF-8"
+                }
+            })
+
+            .then(res => res.json())
+            .then(data => updateUser(data));
+        })
+
+        function updateUser(user){
+            document.getElementById('userName').innerText = user.name;
+        }
+
+
+    </script>
+</body>
+</html>
+
+*/
