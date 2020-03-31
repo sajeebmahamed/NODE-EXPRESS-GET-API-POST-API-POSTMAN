d4a62b7679397749b67bd29d6bb741b76d26e1a4
Sajeeb <sajeebahamed6@gmail.com>
2020-04-01 01:40:09 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     const students = {
@@ -19,4 +25,11 @@ app.get('/users/:id', (req, res) => {
     const name = users[id];
     res.send({id, name});
 })
+//post data
+app.post('/addUser', (req, res) => {
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+});
 app.listen(4200, () => console.log('Listening to the port 4200'));
